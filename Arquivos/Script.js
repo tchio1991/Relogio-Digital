2bb1b82336c52bd8d330dcf41e96d96afee05605
Thiago Cardoso <109465375+tchio1991@users.noreply.github.com>
2022-10-24 17:09:25 -0300

    Updated files

--- Arquivos/Script.js
+++ Arquivos/Script.js
@@ -16,25 +16,25 @@ const relogio = setInterval(function time() {
     if (s < 10) s = '0' + s;
 
     // O background de fundo mudando de cor de acordo com o avanço do tempo
-    if(min > 01 && min < 15 ) {
-        document.body.style.background-color;rgb(0, 4, 255);
-    } else if (min > 15 && min < 20 ) {
-        document.body.style.background-color;rgb(0, 25, 255);
-    } else if (min > 20 && min < 25) {
-        document.body.style.background-color;rgb(0, 40, 255);
-    } else if (min > 25 && min < 30) {
-        document.body.style.background-color;rgb(0, 59, 255);
+    if(min > 00 && min < 10 ) {
+        document.body.style.backgroundColor = "#ffbb64";
+    } else if (min > 10 && min < 20) {
+        document.body.style.backgroundColor = "#ff7d51";
+    } else if (min > 20 && min < 30) {
+        document.body.style.backgroundColor = "#d94935";
     } else if (min > 30 && min < 35) {
-        document.body.style.background-color;rgb(0, 67, 255);
+        document.body.style.backgroundColor = "#a22f28";
     } else if (min > 35 && min < 40) {
-        document.body.style.background-color;rgb(0, 84, 255);
+        document.body.style.backgroundColor = "#801f24";
     } else if (min > 40 && min < 45) {
-        document.body.style.background-color;rgb(0, 89, 255);
+        document.body.style.backgroundColor = "#ffff6e";
     } else if (min > 45 && min < 50) {
-        document.body.style.background-color;rgb(0, 93, 255);
-    } else if (min > 50 && min < 60) {
-        document.body.style.background-color;rgb(0, 99, 255);
-    }
+        document.body.style.backgroundColor = "#ffcf38";
+    } else if (min > 50 && min < 55) {
+        document.body.style.backgroundColor = "#ff892b";
+    } else if (min > 55 && min < 60) {
+        document.body.style.backgroundColor = "#edd35b";
+    } 
 
     horas.textContent = hr;
     minutos.textContent = min;
